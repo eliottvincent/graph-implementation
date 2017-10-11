b8a8b9879773120a34bb58087f111000fbde6bf0
Eliott Vincent <eliott.20100@gmail.com>
2017-10-11 17:00:26 +0200

    first implementation of Numbering class

--- src/numbering.rb
+++ src/numbering.rb
@@ -1,28 +1,32 @@
+require 'set'
+require 'matrix'
+
 class Numbering
-	attr_accessor :counter, :HSI, :nodes
+	attr_accessor :counter, :hsi, :nodes
 
   	def initialize
-		counter = -1
-	  	# HSI = Hash.new
-	  	nodes = Set.new
+		@counter = -1
+	  	@hsi = Hash.new
+	  	@nodes = Set.new
 	end
 
-	def size()
+	def size
 		nodes.length
 	end
 
   	def add_element(node)
-		unless HSI.has_key? :node
-			counter += 1
-			HSI.node = size
-			nodes.push(node)
-			true
+		unless hsi.has_key? :node
+			@counter += 1
+			@hsi[node]= size
+			@nodes.add(node)
+			return true
 		end
+		puts '>>>>>Numbering.add_element: ' + @nodes.to_a.map(&:inspect).to_s
 	  	false
 	end
 
   	def number(node)
-		HSI.node
+		hsi[node]
 	end
 
   	def element_at(i)
